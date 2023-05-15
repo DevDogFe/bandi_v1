95a548e75b08c8c67477b3cc3e1c126e7d4c3001
kimgyeongeun117 <kimgyeoneun117@gmail.com>
2023-05-15 18:42:57 +0900

    contestController 만들기만 함

---
+++ src/main/java/com/bandi/novel/controller/ContestController.java
@@ -0,0 +1,13 @@
+package com.bandi.novel.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 유저 관련 로직 컨트롤러
+ * @author 김지현
+ */
+@Controller
+public class ContestController {
+	
+	 
+}
